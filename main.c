1b633033b24ffaee9d02f20bbd68be6ad578dec2
v1dusss <vsivanat@student.42heilbronn.de>
2024-03-26 21:07:38 +0100

    jgotz is cool :)

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vsivanat <vsivanat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 20:27:04 by vsivanat          #+#    #+#             */
-/*   Updated: 2024/03/26 18:04:43 by vsivanat         ###   ########.fr       */
+/*   Updated: 2024/03/26 21:02:51 by vsivanat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,29 +51,27 @@ void	loop_img(t_master *master)
 
 int	main(int argc, char **argv)
 {
-	t_master	m;
-	t_master	*master;
+	t_master	master;
 	t_fractol	fract;
 	t_pixel		pixel;
 
 	write(1, "0", 1);
-	master = &m;
-	master->fract = &fract;
-	master->pixel = &pixel;
+	master.fract = &fract;
+	master.pixel = &pixel;
 	if (argc < 2 || argc > 4)
 		usage();
 	write(1, "0", 1);
-	master->fract->window_x = 1080;
-	master->fract->window_y = 1080;
+	master.fract->window_x = 1080;
+	master.fract->window_y = 1080;
 	write(1, "0", 1);
-	clear_fract(master->fract, ft_strlower(argv[1]), argc, argv, master);
-	master->mlx = mlx_init(master->fract->window_x, master->fract->window_y,
-			master->fract->fract_name, true);
+	clear_fract(master.fract, ft_strlower(argv[1]), argc, argv, &master);
+	master.mlx = mlx_init(master.fract->window_x, master.fract->window_y,
+			master.fract->fract_name, true);
 	write(1, "0", 1);
-	loop_img(master);
-	mlx_image_to_window(master->mlx, master->img, 0, 0);
+	loop_img(&master);
+	mlx_image_to_window(master.mlx, master.img, 0, 0);
 	// mlx_loop_hook(mlx, mlx_render_images);
-	mlx_key_hook(master->mlx, key_pres, master);
-	mlx_loop(master->mlx);
+	mlx_key_hook(master.mlx, key_pres, &master);
+	mlx_loop(master.mlx);
 	return (0);
 }
